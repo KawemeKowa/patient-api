54652be90480b8c8d4fdec6b83bbf8b380595e0e
Kaweme <68535259+KawemeKowa@users.noreply.github.com>
2021-08-06 17:01:56 +0200

    added patient crud patient api

---
+++ HealthApi/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HealthApi.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Ailment> Ailments{ get; set; }
+        public ICollection<Medication> Medications { get; set; }
+    }
+}
